42f001b13f9fa88f6cdf891d19f8018afbb51b78
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-05-17 16:21:09 +0530

    create navbar and theme context

---
+++ src/pages/ScannerPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar/Navbar.jsx'
+
+const ScannerPage = () => {
+  return (
+    <section>
+        <Navbar />
+    </section>
+  )
+}
+
+export default ScannerPage
